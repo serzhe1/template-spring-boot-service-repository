cfbace56c67fd4c630e46c4ac8caca207708a052
s.ladygin <s.a.ladygin@yandex.ru>
2026-05-24 16:19:17 +0300

    Upgrade dependencies to Spring Boot 4

--- src/main/java/org/renamethis/MainService.java
+++ src/main/java/org/renamethis/MainService.java
@@ -6,10 +6,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class MainService {
 
+    private final Random random;
+
+    public MainService() {
+        this(new Random());
+    }
+
+    MainService(Random random) {
+        this.random = random;
+    }
+
     public void someMethod() {
         System.out.println("someMethod");
 
-        if (new Random().nextInt() % 2 == 0) {
+        if (random.nextInt() % 2 == 0) {
             System.out.println(Thread.currentThread().getName());
         }
     }
--- src/test/java/org/renamethis/TemplateSpringBootServiceRepositoryApplicationTests.java
+++ src/test/java/org/renamethis/TemplateSpringBootServiceRepositoryApplicationTests.java
@@ -1,11 +1,43 @@
 package org.renamethis;
 
+import java.util.Random;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TemplateSpringBootServiceRepositoryApplicationTests {
 
+    @Autowired
+    private MainService mainService;
+
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+        Assertions.assertNotNull(mainService);
+    }
+
+    @Test
+    void mainServiceRunsWhenRandomValueIsEven() {
+        new MainService(new FixedRandom(2)).someMethod();
+    }
+
+    @Test
+    void mainServiceRunsWhenRandomValueIsOdd() {
+        new MainService(new FixedRandom(1)).someMethod();
+    }
+
+    private static final class FixedRandom extends Random {
+
+        private final int value;
+
+        private FixedRandom(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public int nextInt() {
+            return value;
+        }
+    }
 }
